a38457e61516887ec51f464a9ec3cec845a75e2b
jarrett <jarrett@freeformsoftware.dev>
2023-10-25 00:48:05 -0500

    Hi

---
+++ hardware/proxmox-lxc.nix
@@ -0,0 +1,10 @@
+{
+
+  # Supress systemd units that don't work because of LXC.
+  # https://blog.xirion.net/posts/nixos-proxmox-lxc/#configurationnix-tweak
+  systemd.suppressedSystemUnits = [
+    "dev-mqueue.mount"
+    "sys-kernel-debug.mount"
+    "sys-fs-fuse-connections.mount"
+  ]; 
+}
---
+++ systems/garage.nix
@@ -0,0 +1,5 @@
+{
+  services.garage = {
+    enable = true;
+  };
+}
